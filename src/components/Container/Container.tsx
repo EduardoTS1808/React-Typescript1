1184d0c2fa73f63f5f05fb7ae9847e92787b4be4
Eduardo Tavares <eduardo91423344@gmail.com>
2024-06-14 19:26:25 -0300

    type avancados adicionados

--- src/components/Container/Container.tsx
+++ src/components/Container/Container.tsx
@@ -1,11 +1,13 @@
+
+import React, { ReactNode } from 'react';
 import './Container.css';
 
-interface ContainerProps<T> {
+interface ContainerProps<T extends ReactNode> {
   children: T;
   fluid?: boolean;
 }
 
-const Container = <T,>({ children , fluid = false}: ContainerProps<T>): JSX.Element => {
+const Container = <T extends ReactNode>({ children , fluid = false}: ContainerProps<T>): JSX.Element => {
   return <div className={fluid ? 'container-fluid' : 'container'}>{children}</div>;
 };
 
